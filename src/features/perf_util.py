63ffa839860e1d1257201eb9e238e48af95ff5f7
Alex Palcuie <alex.palcuie@ubervu.com>
2014-03-05 14:28:40 +0200

    Change ubervu settings

--- src/features/perf_util.py
+++ src/features/perf_util.py
@@ -1,25 +1,25 @@
 # Please modify this file in order to match your need
 
 predefined = {}
-predefined['id_username'] = "user"
-predefined['id_password'] = "pass"
+predefined['id_username'] = "ubervu"
+predefined['id_password'] = "ubervu"
 
 # define local javascript for loading the file from local
 predefined['local_javascript_url'] = "javascript/local_perf.js"
 
 # define the login page
-predefined['login_url'] = "http://login url"
+predefined['login_url'] = "http://dev.ubervu.local/#/boards/1"
 
 # define the check page
-predefined['check_url'] = "http://page url"
+predefined['check_url'] = "http://dev.ubervu.local/#/boards/1"
 
 #define how many widgets you wish to scroll
-predefined['number_of_widgets'] = 2
+predefined['number_of_widgets'] = 5
 
 #define the step you wish to perform the initial scroll
-predefined['scroll_step'] = 100 # this is the number of px
+predefined['scroll_step'] = 200 # this is the number of px
 
 #define the path to your chromedriver
-predefined['chromedriver'] = ""
+predefined['chromedriver'] = "/usr/local/bin/chromedriver"
 
 # TODO create remote javascript
